38f3429d1b76ff7bb606be0f4c76bb8db645078d
chensenlai <chensenlai@joyy.sg>
2021-09-02 11:33:55 +0800

    文件工具类

--- core/common/src/main/java/com/srobber/common/util/FileUtil.java
+++ core/common/src/main/java/com/srobber/common/util/FileUtil.java
@@ -1,5 +1,22 @@
-package com.srobber.common.util;/**
- *
- * @author  chensenlai
- */public class FileUtil {
+package com.srobber.common.util;
+
+import java.io.File;
+
+/**
+ * 文件操作工具类
+ * @author chensenlai
+ */
+public class FileUtil {
+
+    /**
+     * 生成同名路径其他后缀文件名
+     * @param srcPath 源文件
+     * @param suffix 后缀名
+     * @return 生成文件名
+     */
+    public static String genSuffixFilePath(String srcPath, String suffix) {
+        String fileName = (new File(srcPath)).getName();
+        int lastIndex = fileName.lastIndexOf(".");
+        return lastIndex==-1 ? srcPath+"."+suffix : srcPath.substring(0, srcPath.length()-fileName.length()+lastIndex)+"."+suffix;
+    }
 }
